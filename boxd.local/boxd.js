0a4d499a23af5b65fdf87e4af4c853f44826db02
cing <cinger@github>
2013-08-01 22:39:07 -0700

    commentd, cleand, reorganised

--- boxd.local/boxd.js
+++ boxd.local/boxd.js
@@ -1,95 +1,195 @@
+/////////////////
+///boxd.js
+//uses[DOM,placent(),jQ]
+//is[ jQ{ document.on('click',.on('blur',.on('keyup' } ]
+
 var numb = 0;
 var boxfrac = 4;  //determines the ratio in regard the window to size the current box
+var boxfoc = "";
 
-function boxdheight () {
-											boxheight = (window.outerHeight/(boxfrac))*(boxfrac-1);   //focusd box
-}
-function shrunkdheight() {
-                shrunkheight = boxheight/7;
-                if ( shrunkheight < 17 ){
-                  shrunkheight = 17;
-                } // this way at least a single line is visible
-}
+
+var game = "";
+var vald = 17;  //used to nullify consecutive clicks on an already focused textbox,17 or undf
+
+
+$(document).on('click','textarea',function() {
+  if ( vald ) {
+    $(this).animate({ height: boxheight }, 1000,function(){
+      $('p.anot').text($('#'+this.id).position().top+".."+vald+',.,'+(numb+4)+";box: "+boxheight+";shrunk: "+shrunkheight);
+      placent(this);
+    });
+  }
+  boxfoc = this;
+  vald='';
+  $('p.boxy').text('boxy: ' + $('#'+boxfoc.id).position().top);
+}); // .on('click', expand and place in center
+
+
+$(document).on('blur','textarea',function() {
+  vald=17;
+  game="";
+  $(this).animate({ height: shrunkheight }, 980);  // 17*3=51
+}); // .on('blur', shrink box
+
+
+$(document).on('keyup','textarea', function(event) {
+  game = this.id;     //replace this with a get.object.value 'game' from current box
+  console.log(game);
+	
+  var keyd = event.keyCode || event.which;
+  var pushd = String.fromCharCode(keyd);
+	
+  if ( typeof window[game] == 'function'){					
+    window[game](this,keyd,pushd);
+  }  
+}); // .on('keyup', when keypress go to current box's ruleset
+
+
+
+
+/////////////////
+///butt.js
+//is[infd(),topd(),store(),addele(),remele()]
+
+function infd() {   //uses[DOM]
+  window.scrollTo(0,document.body.scrollHeight);
+} //infd(), show info, via scroll
+
+function topd() {   //uses[DOM]
+  window.scrollTo(0,0);
+} //topd(), scroll to top
+
+
+
+function store() {   //uses[stord(),jQ]
+ //localStorage.clear();
+console.log($('#theflow').children().length-4);
+ $('#theflow > textarea').each(function() {
+console.log(this.id);
+    localStorage[this.id]=this.value;
+    stord();
+ }); // for each textbox store its value to the local storage, then call stord for an overview of all the data
+} // store(), store the values into db
+
+
+function addele() {   //uses[v{+}numb,DOM]
+  var flow = document.getElementById('theflow');
+  var num = ( numb ) + 1;
+  var romu = romanise(num);
+  var newbox = document.createElement('textarea');
+  var newboxid ='box'+num;
+
+  newbox.setAttribute('id',newboxid);
+  newbox.setAttribute('class','tecksd');
+  newbox.setAttribute('placeholder','('+romu+')');
+  flow.appendChild(newbox);
+
+  numb = num;
+
+  if ( localStorage[newboxid] ) {
+    newbox.value=localStorage[newboxid];
+  }
+
+} //addele(), add element
+
+
+
+function remele(){ //uses DOM
+  var flow = document.getElementById('theflow');
+  flow.removeChild(boxfoc);
+} // remele(), remove element
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////
+///wind.js
+//is[ boxdheight(), shrunkdheight() ]
+//uses[ padd() ]
 
 var boxheight = (window.outerHeight/(boxfrac))*(boxfrac-1);   //focusd box
+
+padd(boxheight);
+
 var shrunkheight = boxheight/7;         //unfocusd box
-	if ( shrunkheight < 17 ){
+	if ( shrunkheight < 17 ) {
 		shrunkheight = 17;
 	} // this way at least a single line is visible
 
-var game = "";
-var boxfoc = "";
-var olebox = "";
+function boxdheight () { //uses[ DOM, padd() ]
+  boxheight = (window.outerHeight/(boxfrac))*(boxfrac-1);   //focusd box
+	padd(boxheight);
+} // boxdheight(), determines boxheight and modifies the pads
+
+function shrunkdheight() {
+  shrunkheight = boxheight/7;
+  if ( shrunkheight < 17 ) {
+    shrunkheight = 17;
+  } // this way at least a single line is visible
+} //shrunkdheight(), determines the desired shrunk height
+
+
+
+
+
+
+/////////////////
+///css.js
+//is[ padd(), jQ{ #whereitsat.scroll ; thelot.scroll } ]
+
 $('#whereitsat').scroll(function() {
-				document.body.style.overflow='hidden';
+  document.body.style.overflow='hidden';
 });
+
 $('thelot').scroll(function() {
-				document.body.style.overflow='scroll';
+  document.body.style.overflow='scroll';
 });
 
-var vald = 17;  //used to nullify consecutive clicks on an already focused textbox,17 or undf
-	//boxheight=window.innerHeight/2;
-	$('p.padittop').css('padding-top',boxheight+'px');
-	$('p.paditbot').css('padding-bottom',boxheight+'px');
 
-$(document).on('click','textarea',function() {
-	$('p.padittop').css('padding-top',boxheight+'px');
-	$('p.paditbot').css('padding-bottom',boxheight+'px');
-					if ( vald ) {
-						$(this).animate({ height: boxheight }, 1000,function(){
-						$('p.anot').text($('#'+this.id).position().top+".."+vald+',.,'+(numb+4)+";box: "+boxheight+";shrunk: "+shrunkheight);
-						
-						
-						var postop = $('#'+this.id).position().top;
-					  var totwin = window.outerHeight;
-						var fracwin = totwin/(boxfrac);
-						var totbox = boxheight;
-					  var fracbox = totbox/(boxfrac);
-						var offbox = fracbox+postop;
-						var offwin = fracwin-offbox;
-						var centd = $('#foryou.scrolld').scrollTop()-offwin;
-						$('#foryou.scrolld').animate({ scrollTop: centd },700);
+function padd(padheight) { // uses[jQ]
+  $('p.padittop').css('padding-top',padheight+'px');
+  $('p.paditbot').css('padding-bottom',padheight+'px');
+} // padd(), pad theflow so at full scroll at least one box is stil visible
 
-						});
-					}
-						olebox = boxfoc;
-						boxfoc = this;
-						vald='';
-    $('p.boxy').text('boxy: ' + $('#'+boxfoc.id).position().top);
-});
+
+
+
+
+
+/////////////////
+///display.js
+//is[ jQ{ #foryou.scroll } ]
 
 $('#foryou').scroll(function() {
 				$('p.dispy').text($('#foryou').scrollTop()+' .. pads : t:  : b :'+$('p.padittop').css("padding-top"));
 });
 
-$(document).on('blur','textarea',function() {
-					vald=17;
-					game="";
-					$(this).animate({ height: shrunkheight }, 980);  // 17*3=51
-	//$('p.padittop').css('padding-top',0+'px');
-	//$('p.paditbot').css('padding-bottom',0+'px');
-}); // .on('blur', shrink box
 
 
-function getcursor(el) {
-						if (el.selectionStart) {
-							return el.selectionStart;
-							} else if (document.selection) {
-										el.focus();
-	
-										var r = document.selection.createRange();
-										if ( r == null ){
-													return 0;
-										}
-										var re = el.createTextRange(),
-												rc = re.duplicate();
-										re.moveToBookmark(r.getBookmark());
-										rc.setEndPoint('EndToStart',re);
-										return rc.text.length;
-							}
-						 return 0;
 
-} // getcursor(), finds cursor location in element
+
+
+
+
+
+
+
+/////////////////
+///game.js
+//is[ boxd() ]
+//xuses
+
 
 function boxd(thisbox,keyd,pushd) {
 			var cursdisp = 0;
@@ -188,98 +288,76 @@ function boxd(thisbox,keyd,pushd) {
 } //boxd(), boxd ruleset
 
 
-$(document).on('keyup','textarea', function(event) {
-				game = this.id;     //replace this with a get.object.value 'game' from current box
-				console.log(game);
-					
-				var keyd = event.keyCode || event.which;
-				var pushd = String.fromCharCode(keyd);
-				
-				if ( typeof window[game] == 'function'){					
-					window[game](this,keyd,pushd);
-				}
-}); // .on('keyup', when keypress go to current box's ruleset
 
 
 
-function stord() {
-                var stord = unescape(encodeURIComponent(JSON.stringify(localStorage))).length/1024/1024;
-                var shortd = stord.toFixed(3);
-                $('p.stord').text('store: ' + shortd + 'mb');
-                var fivemegworthchars = 5000000;
-                var fivemegworthlines = 65000;
-                var liney=fivemegworthlines - shortd*fivemegworthlines;
-                var chardy=fivemegworthchars - shortd*fivemegworthchars;
-                $('p.tona').text('lines left: ' + liney + '..characters left: ' + chardy);
+
+
+
+
+
+
+
+
+
+
+/////////////////
+//pure.js
+//is[placent(), stord(), thesets(), getcursor(), romanise() ]
+
+
+function placent(thisbox) { // uses[ DOM, jQ{ #foryou.scrolld.animate } ]
+  var postop = $('#'+thisbox.id).position().top;
+  var totwin = window.outerHeight;
+  var fracwin = totwin/(boxfrac);
+  var totbox = boxheight;
+  var fracbox = totbox/(boxfrac);
+  var offbox = fracbox+postop;
+  var offwin = fracwin-offbox;
+  var centd = $('#foryou.scrolld').scrollTop()-offwin;
+  $('#foryou.scrolld').animate({ scrollTop: centd },700);
+} // place this in center of window
+
+
+
+function stord() { //uses[ jQ{ pstord.text ; ptona.text } ]
+  var stord = unescape(encodeURIComponent(JSON.stringify(localStorage))).length/1024/1024;
+  var shortd = stord.toFixed(3);
+  $('p.stord').text('store: ' + shortd + 'mb');
+  var fivemegworthchars = 5000000;
+  var fivemegworthlines = 65000;
+  var liney=fivemegworthlines - shortd*fivemegworthlines;
+  var chardy=fivemegworthchars - shortd*fivemegworthchars;
+  $('p.tona').text('lines left: ' + liney + '..characters left: ' + chardy);
 } // stord(), determine how much is stored in db
 
-function infd() {
-					window.scrollTo(0,document.body.scrollHeight);
-} //infd()
 
-function topd() {
-					window.scrollTo(0,0);
-} //topd(), scroll to top
 
-function thesets() { //uses boxheight, shrunkheight
+function thesets() { //uses[ boxheight(), shrunkheight(), jQ{ p.text } ]
 								boxdheight();
 								shrunkdheight();
-                $('p.padittop').css('padding-top',boxheight+'px');
-                $('p.paditbot').css('padding-bottom',boxheight+'px');
-
-
                 $('p.dyhei').text('hei : '+ boxheight);
-
-                //$('#foryou.scrolld').css('height',(boxheight*2-33)+'px');
 } // thesets(), sets textarea dimensions
 
 
-function store() {
 
-                //localStorage.clear();
-
-                console.log($('#theflow').children().length-4);
-
-
-                $('#theflow > textarea').each(function() {
-                	console.log(this.id);
-                	localStorage[this.id]=this.value;
-                	stord();
-                });
-} // store(), store the values into db
-
-
-function addele() {    //uses +numb,DOM
-
-                var flow = document.getElementById('theflow');
-                var num = ( numb ) + 1;
-                var romu = romanise(num);
-
-                var newbox = document.createElement('textarea');
-                var newboxid ='box'+num;
-
-                newbox.setAttribute('id',newboxid);
-                newbox.setAttribute('class','tecksd');
-                newbox.setAttribute('placeholder','('+romu+')');
-
-                flow.appendChild(newbox);
-
-                numb = num;
-
-                if ( localStorage[newboxid] ){
-                  newbox.value=localStorage[newboxid];
-                }
-
-} //addele(), add element
-
-
-
-function remele(){ //uses DOM
-                var flow = document.getElementById('theflow');
-                flow.removeChild(boxfoc);
-} // remele(), remove element
-
-
+function getcursor(el) { //uses[DOM]
+						if (el.selectionStart) {
+							return el.selectionStart;
+							} else if (document.selection) {
+										el.focus();
+										var r = document.selection.createRange();
+										if ( r == null ){
+													return 0;
+										}
+										var re = el.createTextRange(),
+												rc = re.duplicate();
+										re.moveToBookmark(r.getBookmark());
+										rc.setEndPoint('EndToStart',re);
+										return rc.text.length;
+							}
+						 return 0;
+} // getcursor(), finds cursor location in element
 
 
 
@@ -296,23 +374,6 @@ function romanise (num) { //inde
                     roman = (key[+digits.pop() + (i * 10)] || "") + roman;
                 return Array(+digits.join("") + 1).join("M") + roman;
 } // romanise(), turn numbers into roman numerals
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
